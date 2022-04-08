78923c04738998e18a683d805f0ddb16ba34ea9a
Farzan-I <farzan_15@hotmail.co.uk>
2022-04-08 13:09:13 +0100

    Spec updated

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,5 +4,17 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do 
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
 end
 
+# number % 3 == 0 = "fizz"
+# number % 5 == 0 = "buzz"
+# number % 3 == 0 && number % 5 == 0 = "fizzbuzz"
