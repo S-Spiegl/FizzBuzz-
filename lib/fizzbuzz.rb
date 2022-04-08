807b7a1788923f8de631b24f94896ae496d9e4fc
StevenSpiegl <steven.spiegl@gmail.com>
2022-04-08 14:56:00 +0100

    errorerrorerror

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,18 @@
 def fizzbuzz(number)
-  'fizz'
+  (1..number).map do |num|
+  # num = 0
+  # while num < 21
+    # num += 1
+    if num % 3 == 0 && num % 5 != 0
+      puts 'fizz'
+    elsif num % 5 == 0 && num % 3 != 0
+      puts 'buzz'
+    elsif num % 3 == 0 && num % 5 == 0
+      puts 'fizzbuzz'
+    else
+      puts num
+    end
+  end
 end
+
+fizzbuzz(15)
